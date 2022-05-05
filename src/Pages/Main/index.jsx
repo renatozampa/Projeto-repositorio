b6a0e0979a149a9fd1f5ecf9915a6defd4bc2c6d
Renato Zampa <75633662+renatozampa@users.noreply.github.com>
2022-05-04 21:18:04 -0300

    Add files via upload

---
+++ src/Pages/Main/index.jsx
@@ -0,0 +1,9 @@
+import { Title } from "./styles";
+
+export default function Main() {
+    return(
+        <div>
+            <Title>ola Main</Title>
+        </div>
+    )
+}
---
+++ src/Pages/Repositorio/index.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Repositorio() {
+    return (
+        <div>
+            <h1>ola Repositorio</h1>
+        </div>
+    )
+}
